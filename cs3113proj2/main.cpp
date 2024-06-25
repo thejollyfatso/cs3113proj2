a18eced660e2a563f7ce180c4bfbbd0ce2c1c36d
thejollyfatso <rafael.r.deleon@gmail.com>
2024-06-25 19:10:29 -0400

    header inserted for submission

--- cs3113proj2/main.cpp
+++ cs3113proj2/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Rafael de Leon
+* Assignment: Pong Clone
+* Date due: 2024-06-29, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
